d8826bb68e3af6e66779db9e4db2dac372274e82
alucas69 <a.lucas@vu.nl>
2020-06-20 16:40:13 +0200

    small bug fixes

--- write_quiz_canvas_alph.R
+++ write_quiz_canvas_alph.R
@@ -1,12 +1,12 @@
-#' construct Canvas xml code for an upload question
+#' construct Canvas xml code for an essay question
 #'
-#' This function constructs Canvas xml code for an upload question
+#' This function constructs Canvas xml code for an essay question
 #' @param question is a list structure with $text the question text,  $points_possible the number of points for this question (if absent, then 1pt).
 #' @param answer_counter is an integer, used to identify subanswers in other type questions.
 #' @param question_key is an identifier for the question. If not present, a random key is generated using generate_key().
-#' @keywords upload question for canvas quiz
+#' @keywords essay question for canvas quiz
 #' @export list A list structure holding $output a vector of strings with the <xml> code for the question, and $answer_counter the updated answer_counter.
-write_quiz_canvas_upl= function(question, answer_counter, question_key=NULL) {
+write_quiz_canvas_alph= function(question, answer_counter, question_key=NULL) {
   
   # check html correctness of question text
   simple_html_checker(question$text)
@@ -25,7 +25,7 @@ write_quiz_canvas_upl= function(question, answer_counter, question_key=NULL) {
   output2= write_in_wrapper(
     c(
       write_in_wrapper("question_type", "fieldlabel"),
-      write_in_wrapper("file_upload_question", "fieldentry")
+      write_in_wrapper("essay_question", "fieldentry")
     ), "qtimetadatafield", block=TRUE
   )
   output= c(output, output2)
@@ -71,15 +71,20 @@ write_quiz_canvas_upl= function(question, answer_counter, question_key=NULL) {
   # add wrappers for canvas
   output2= write_in_wrapper(output2, "mattext", s_wrappertag="texttype=\"text/html\"", block=TRUE)
   output2= write_in_wrapper(output2, "material", block=TRUE)
+  
+  # add response part
+  output3= write_in_wrapper("<response_label ident=\"answer1\" rshuffle=\"No\"/>", "render_fib", block=TRUE)
+  output2= c(output2, write_in_wrapper(output3, "response_str", s_wrappertag="ident=\"response1\" rcardinality=\"Single\"", block=TRUE))
   output2= write_in_wrapper(output2, "presentation", block=TRUE)
   output= c(output, output2)
   
   # add upload field in canvas wrappers
   output2= "<decvar maxvalue=\"100\" minvalue=\"0\" varname=\"SCORE\" vartype=\"Decimal\"/>"
   output2= write_in_wrapper(output2, "outcomes", block=TRUE)
+  output2= c(output2, write_in_wrapper(write_in_wrapper("<other/>", "conditionvar", block=TRUE), "respcondition", s_wrappertag="continue=\"No\"", block=TRUE))
   output2= write_in_wrapper(output2, "resprocessing", block=TRUE)
   output= c(output, output2)
-  
+
   # add total question wrapper
   output= write_in_wrapper(output, "item", s_wrappertag=sprintf("ident=\"%s\" title=\"%s\"", question_key, question$q), block=TRUE)
   return(list(output=output, answer_counter=answer_counter))
