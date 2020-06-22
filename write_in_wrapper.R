156aff85f5ca53a5c5872cfd236debc2c5c1ab4b
alucas69 <a.lucas@vu.nl>
2020-06-22 15:24:54 +0200

    bug fix: eleminate warning message for vector-tag case

--- write_in_wrapper.R
+++ write_in_wrapper.R
@@ -3,12 +3,15 @@
 # if block == TRUE, write the whole vector of strings into the wrapper (rather than each
 # element separately)
 write_in_wrapper= function(vs_text, s_wrappername, s_wrappertag="", inline=TRUE, block=FALSE) {
-  if (stri_length(s_wrappertag) > 0) s_wrappertag= paste(" ", s_wrappertag, sep="")
+  # add preceding space for present tags
+  s_wrappertag= paste(c(""," ")[1+(stri_length(s_wrappertag) > 0)], s_wrappertag, sep="")
+  # in block, indent within wrapper
   if (block) return(c(
     sprintf("<%s%s>", s_wrappername, s_wrappertag),
     sprintf_indent(vs_text),
     sprintf("</%s>", s_wrappername)
   )) else {
+    # otherwise inline wrapping
     if (inline) return(sprintf("<%s%s>%s</%s>", s_wrappername, s_wrappertag, vs_text, s_wrappername))
     else return(rbind(
       sprintf("<%s%s>", s_wrappername, s_wrappertag),
