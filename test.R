92d45356a862367960a4a6b2d31a6755b47159b0
alucas69 <a.lucas@vu.nl>
2020-06-22 21:30:23 +0200

    extend: write complete canvas and html quiz

--- test.R
+++ test.R
@@ -29,4 +29,54 @@ engine_sources= c(
 )
 for (subsource in engine_sources) eval(parse(text=sprintf("source(\"%s\")", subsource)))
 
-write_quiz_html_test()
+
+# set up the quiz
+qdir= "./2020resitv_1/questions"
+numbervariations= 1
+questions= matrix(c(
+  numbervariations, "Q20200629_1a", 
+  numbervariations, "Q20200629_1b", 
+  numbervariations, "Q20200629_1c",
+  numbervariations, "Q20200629_1d",
+  numbervariations, "Q20200629_1e",
+  numbervariations, "Q20200629_1g",
+  numbervariations, "Q20200629_1h",
+  numbervariations, "Q20200629_1i",
+  numbervariations, "Q20200629_1j",
+  numbervariations, "Q20200629_1l",
+  numbervariations, "Q20200629_1m"
+), nrow=2)
+
+# load the sources
+for (blockcounter in 1:ncol(questions)) {
+  print(sprintf("source(\"%s/%s.R\")", qdir, questions[2, blockcounter]))
+  eval(parse(text = sprintf("source(\"%s/%s.R\")", qdir, questions[2, blockcounter])))
+}
+
+# draw the quiz
+exam = list()
+for (blockcounter in 1:ncol(questions)) {
+  block = list()
+  for (questioncounter in 1:questions[1, blockcounter]) {
+    print(sprintf("Question %d.%d", blockcounter, questioncounter))
+    eval(parse(text = sprintf("question_tmp= %s()", questions[2, blockcounter])))
+    question_tmp$q= sprintf("Q%d", blockcounter)
+    block= append(block, list(question_tmp))
+    
+    #check html
+    print("Checking html of question")
+    print(question_tmp$text)
+    simple_html_checker(question_tmp$text)
+    print("Checking html of answer")
+    if ((question_tmp$type == "ma") | (question_tmp$type == "mb") | (question_tmp$type == "mc")) simple_html_checker(question_tmp$answer)
+  }
+  exam= append(exam, list(block))
+}
+
+# write the quiz
+write_quiz_html(exam)
+write_quiz_canvas(exam)
+
+
+
+
