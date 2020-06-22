4a6955c1c80437cf097f18c53a1188f188da1973
alucas69 <a.lucas@vu.nl>
2020-06-22 21:33:55 +0200

    update

--- TODOLIST.R
+++ TODOLIST.R
@@ -2,3 +2,5 @@
 #   Latex2html only does the html step, not the escape step
 #   Write mb routine 
 #   add mb to maketestquiz
+#   make library
+#   disentange aidfunctions and other cores in separate files
