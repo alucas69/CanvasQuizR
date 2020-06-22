8d923a1d8ddf43974c415e64f47ff682a9720205
alucas69 <a.lucas@vu.nl>
2020-06-22 21:33:36 +0200

    update: write questions in new list format

--- 2020resitv_1/questions/Q20200629_1g.R
+++ 2020resitv_1/questions/Q20200629_1g.R
@@ -1,39 +1,26 @@
-Q20200629_1g <- function(QuizFile, Counters, NumberOfVariations) {
+Q20200629_1g <- function() {
   
-  for (i1 in 1:NumberOfVariations) {
-    #
-    # though asking # decimals, we are more generous ...
-    #
-    iDigits <- 4
-    #
-    # generate random mu, sigma2, and quantile
-    #
-    iN <- sample(c(5:7), 1)
-    dPi <- sample(c(0.05, 0.15, (1:7)/10), 1)
-    #
-    # formulate the question
-    #
-    QuestionText <- c(
-      sprintf(
-        "You sell high end luxury goods. The probability that you close at least one 
+  # generate random mu, sigma2, and quantile
+  iDigits= 4
+  qtype= "num"
+  iN= sample(c(5:7), 1)
+  dPi= sample(c(0.05, 0.15, (1:7)/10), 1)
+  
+  # formulate the question
+  QuestionText <- c(
+    tex2math(sprintf(
+      "You sell high end luxury goods. The probability that you close at least one 
         sale is $\\pi = %d%%$ per day. Assume that sales are independent across days
         and that you are open %d days a week. Compute the probability of having no 
         sales in a particular week in %d decimal places.",
-        round(dPi*100, digits = 0), iN, iDigits
-      ),
-      DigitDecimalWarning(iDigits)
-    )
-    #
-    # compute the answer and error margin
-    #
-    Answers <- matrix((1-dPi)^iN + 0.0002 * c(0, -1, 1), 1, 3)
-    #
-    # pose the question
-    #
-    Counters <- NUMquestion(QuizFile, "(prob. calculation)", QuestionText,
-                           Answers, Counters, texify=TRUE)
-  }
+      round(dPi*100, digits = 0), iN, iDigits
+    )),
+    DigitDecimalWarning(iDigits)
+  )
   
-  return(Counters)
+  # compute the answer and error margin
+  Answers <- matrix((1-dPi)^iN + 0.0002 * c(0, -1, 1), ncol=3)
+  
+  return(list(type=qtype, text=QuestionText, answer=Answers))    
 }
 
--- 2020resitv_1/questions/Q20200629_1l.R
+++ 2020resitv_1/questions/Q20200629_1l.R
@@ -1,39 +1,26 @@
-Q20200629_1l <- function(QuizFile, Counters, NumberOfVariations) {
+Q20200629_1l <- function() {
   
-  for (i1 in 1:NumberOfVariations) {
-    #
-    # though asking # decimals, we are more generous ...
-    #
-    iDigits <- 3
-    #
-    # generate random mu, sigma2, and quantile
-    #
-    iN <- sample(10:30, 1)
-    dR <- sample( c(-90:-15, 15:90)/100, 1)
-    iRdigits <- 2
-    #
-    # formulate the question
-    #
-    QuestionText <- c(
-      sprintf("Given is a sample of size %d of observations of the two variables $X$ 
+  # generate random mu, sigma2, and quantile
+  iDigits= 3
+  qtype= "num"
+  iN= sample(10:30, 1)
+  dR= sample( c(-90:-15, 15:90)/100, 1)
+  iRdigits= 2
+  
+  # formulate the question
+  QuestionText <- c(
+    tex2math(sprintf("Given is a sample of size %d of observations of the two variables $X$ 
       and $Y$, we computed a sample correlation of $r = %4.2f$. We test the 
       hypothesis $H_0: \\rho=0$ against a 2-sided alternative using our standard 
       test statistic for this purpose. Compute the value of this test statistic 
       in %d decimals.",
-      iN, dR, iDigits),
-      DigitDecimalWarning(iDigits)
-    )
-    #
-    # compute the answer and error margin
-    #
-    Answers <- dR / sqrt((1 - dR*dR) / (iN-2))
-    Answers <- matrix(Answers + 0.002 * c(0, -1, 1), 1, 3)
-    #
-    # pose the question
-    #
-    Counters <- NUMquestion(QuizFile, "(probability calculation)", QuestionText, 
-                            Answers, Counters, texify = TRUE)
-  }
+                     iN, dR, iDigits)),
+    DigitDecimalWarning(iDigits)
+  )
   
-  return(Counters)
+  # compute the answer and error margin
+  Answers <- dR / sqrt((1 - dR*dR) / (iN-2))
+  Answers <- matrix(Answers + 0.002 * c(0, -1, 1), ncol=3)
+  
+  return(list(type=qtype, text=QuestionText, answer=Answers))    
 }
