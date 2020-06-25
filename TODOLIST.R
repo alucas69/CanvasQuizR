0dcd06c7f373035e2e2ea376f5d7d6f0b0f331f0
alucas69 <a.lucas@vu.nl>
2020-06-25 08:13:27 +0200

    update: made new todo's

--- TODOLIST.R
+++ TODOLIST.R
@@ -1,8 +1,14 @@
 # New set-up:
 #   Latex2html only does the html step, not the escape step
 #   Write mb routine 
-#   add mb to maketestquiz
+#   write single blank routine
+#   write pulldown and multiple pulldown routine
+#   write formula question routine
+#   add new question types to maketestquiz
 #   make library
 #   disentange aidfunctions and other cores in separate files
-#   Check everywhere that sampling from a scalar is the scalar, and not a random number 1...int(scalar)
+#   Check everywhere that sampling from a
+#       a random number 1...int(scalar); 
+#       use vectorsample()
 #   iAlt in answer_select works incorrectly, if I give 7, it gives 8 options plus noneoftheabove
+#   put check on non-asci in html checker
