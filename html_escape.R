ce548b10742e51b68ebabb928ac065d157128ecd
alucas69 <a.lucas@vu.nl>
2020-06-25 15:19:31 +0200

    bug fix: convert_ampersand now working vector-valued

--- html_escape.R
+++ html_escape.R
@@ -1,5 +1,5 @@
 html_escape= function(vs_text, convert_ampersand=TRUE, removenonascii=TRUE) {
-  if (convert_ampersand) vs_text[i1]= stri_replace_all(vs_text[i1], "&amp;", regex = "&")
+  if (convert_ampersand) vs_text= stri_replace_all(vs_text, "&amp;", regex = "&")
   vs_text= stri_replace_all(vs_text, "&lt;", regex = "<")
   vs_text= stri_replace_all(vs_text, "&gt;", regex = ">")
   # the line below is not 100% tested for canvas yet ...
