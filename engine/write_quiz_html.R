e4c1a090852d777860b4bcae3cd45843c0d85567
alucas69 <a.lucas@vu.nl>
2020-06-26 12:19:34 +0200

    extend: write identifiers, and use same identifier as canvas quiz if set outside in quiz

--- engine/write_quiz_html.R
+++ engine/write_quiz_html.R
@@ -1,19 +1,34 @@
-# l_quiz is a quiz list of blocks, each block a list of questions
-write_quiz_html= function(l_quiz, s_filename="write_quiz_html.html", subdir=".", texify=TRUE) {
+write_quiz_html_h1warning= function(x, message) {
+  if (is.null(x)) output= write_in_wrapper(paste(message, "(not set)"), "h1", s_wrappertag = "style=\"color:red\"")
+  else output= write_in_wrapper(paste(message, x), "h1")
+  return(output)
+}
+                                     
 
-  # print the identifier and emergency message
-  if (is.null(l_quiz$seed)) output= write_in_wrapper("Exam ID: (not set)", "h1", s_wrappertag = "style=\"color:red\"")
-  else output= write_in_wrapper(paste("Exam ID:", l_quiz$seed), "h1")
-  if (is.null(l_quiz$emergency_html) | (stri_length(l_quiz$emergency_html)==0)) 
-    output= c(output, write_in_wrapper("Emergency message: (not set)", "h1", s_wrappertag = "style=\"color:red\""))
-  else 
-    output= c(output, write_in_wrapper("Emergency message:", "h1"),
-              write_in_wrapper(l_quiz$emergency_html, "p"))
+# l_quiz is a quiz list of blocks, each block a list of questions
+write_quiz_html= function(l_quiz, s_filename="", subdir=".", texify=TRUE) {
   
+  # set filename
+  if (stri_length(s_filename) > 0) s_filename= paste(l_quiz$key, s_filename, sep="_")
+  else if (!is.null(l_quiz$key)) s_filename= paste(l_quiz$key, ".html", sep="")
+  else s_filename= "write_quiz.html"
+
+  # print the name
+  output= NULL
+  output= c(output, write_quiz_html_h1warning(l_quiz$name, "Quiz name:"))
+  output= c(output, write_quiz_html_h1warning(l_quiz$seed, "Quiz rnd seed:"))
+  output= c(output, write_quiz_html_h1warning(l_quiz$key, "Quiz canvas identifier key:"))
+  output= c(output, write_quiz_html_h1warning(l_quiz$emergency_html, "Quiz canvas emergency message:"), 
+            paste("<p>", l_quiz$emergency_html, "</p>"))
+
   # compile the quiz
   for (i0 in 1:length(l_quiz$blocks)) {
     block= l_quiz$blocks[[i0]]
-    output= c(output, write_in_wrapper(write_in_wrapper(sprintf("Here starts block %d", i0), "strong"), "h1"))
+    output= c(
+      output, 
+      write_in_wrapper(
+        write_in_wrapper(sprintf("Here starts block %s", paste("\"", block$name, "\"", "(number ", i0, ")")), "strong"), 
+        "h1"))
     for (i1 in 1:length(block$questions)) {
       question= block$questions[[i1]]
       if (texify) {
